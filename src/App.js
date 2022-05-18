231f95b88113f5401989d07aea3b9b447b30df24
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-18 23:20:38 +0600

    done navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { Route, Routes } from 'react-router-dom';
+import Header from './Pages/Header';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-center uppercase text-4xl font-bold text-purple-500 mt-10">
+        Welcome To TODO Web Application
+     </h2>
+     <div class="card w-96 mx-auto mt-20 bg-purple-500 text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title"> Welcome to you to our todo world. In our life we have to do many task at a time. Most of the time we get confused and lost our focus on which task we have to complete first and which task next. So For you we have come with a solution to manage your task effectively and efficiently. In this todo app you can create your task and complete them with more focus and effective manner. So let's not waste time anymore. Create your account and make your TODO LIST </h2>
+    <p></p>
+    
+  </div>
+</div>
+       
+    </div>
+  );
+};
+
+export default Home;
